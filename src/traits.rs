57db80b0c8240eda132f71a0718258325bb8bb1d
Léo Gaspard <leo@gaspard.io>
2025-03-04 00:15:18 +0100

    introduce BoxFuture type alias

--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,6 @@
 use futures_util::Stream as FutStream;
 use std::future::Future as StdFuture;
+use std::pin::Pin;
 
 crate::dispatch_to_submodules!();
 
@@ -10,3 +11,8 @@ impl<T: Send + StdFuture> Future for T {}
 /// A stream that is `Send` on native, but `!Send` on web
 pub trait Stream: Send + FutStream {}
 impl<T: Send + FutStream> Stream for T {}
+
+/// An owned dynamically typed `Future` for use in cases where you can’t statically type your result or need to add some indirection.
+///
+/// Automatically `Send` on non-wasm32, and not `Send` on wasm32.
+pub type BoxFuture<'a, T> = Pin<Box<dyn 'a + Future<Output = T>>>;
