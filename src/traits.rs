d57fa53a7b99ead7b57a3229543cc95ee5dc42bf
Léo Gaspard <leo@gaspard.io>
2025-03-06 00:37:48 +0100

    introduce BoxStream type alias

--- src/traits.rs
+++ src/traits.rs
@@ -16,3 +16,8 @@ impl<T: Send + FutStream> Stream for T {}
 ///
 /// Automatically `Send` on non-wasm32, and not `Send` on wasm32.
 pub type BoxFuture<'a, T> = Pin<Box<dyn 'a + Future<Output = T>>>;
+
+/// An owned dynamically typed `Stream` for use in cases where you can’t statically type your result or need to add some indirection.
+///
+/// Automatically `Send` on non-wasm32, and not `Send` on wasm32.
+pub type BoxStream<'a, T> = Pin<Box<dyn 'a + Stream<Item = T>>>;
